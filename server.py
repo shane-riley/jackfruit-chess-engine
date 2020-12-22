7c12f596a72171ca94d6d808ae854a437e886e52
Nick Zullo <njz12@pitt.edu>
2020-12-21 19:39:14 -0500

    Add Game Logic

--- server.py
+++ server.py
@@ -1,10 +1,23 @@
-from flask import Flask, render_template, redirect, url_for
+from flask import Flask, render_template, redirect, url_for, request
 
 app = Flask(__name__)
+fen = ''
 
 @app.route('/')
 def board():
 	return render_template('index.html')
+
+@app.route('/fen', methods=['GET', 'POST'])
+def getFen():
+	global fen
+	if request.method == 'POST':
+		fen = request.get_json()['fen']
+		print(fen)
+		return "OK", 200
+	if request.method == 'GET':
+		return {'fen' : fen}, 200
+	
+	return "<h1>Not For You!</h1>", 403
 
 if __name__ == "__main__":
 	app.run()
--- static/js/index.js
+++ static/js/index.js
@@ -1,19 +1,85 @@
-var board = Chessboard('board', {
-	draggable : true,
-	position : 'start'
-});
+var board = null;
+var game = null;
+function load(){
+	var end_banner = document.getElementById("end-banner");
+	var ending = document.createElement("h1");		//Add the end of game message to the top
+	ending.id = "ending-banner";
+	ending.style.display = "none";					//Initially hidden
+	end_banner.appendChild(ending);
+	board = Chessboard('board', {		//Create the board GUI
+		draggable : true,			//Allow the pieces to be moved
+		position : 'start',			//Create the board in starting position
+		onDrop : onDrop,				//Run the onDrop function after letting go of a piece on the GUI
+		onChange : onChange
+	});
+	game = new Chess();					//Create the game logic
+	var button_container = document.getElementById("btn-container");	//Buttons for actions
+	var clear = document.createElement("button");		//Clears the board
+	clear.id = "btn-clear";
+	clear.textContent = "Clear Board";
+	clear.onclick = function() {
+		board.clear();
+		newGame();
+	}
+	var start = document.createElement("button");		//Resets back to start positions
+	start.id = "btn-start";
+	start.textContent = "Start Position";
+	start.onclick = function() {
+		board.start();
+		newGame();
+	}
+	button_container.appendChild(clear);
+	button_container.appendChild(start);
+	var fen_btn = document.createElement("button");		//temporary to send fen to server
+	fen_btn.textContent = "Send Fen to Server"
+	fen_btn.onclick = sendFen;
+	button_container.appendChild(fen_btn);
+}
 
-var button_container = document.getElementById("btn-container");
-var clear = document.createElement("button");
-clear.id = "btn-clear";
-clear.textContent = "Clear Board";
-clear.onclick = board.clear;
-var start = document.createElement("button");
-start.id = "btn-start";
-start.textContent = "Start Position";
-start.onclick = board.start;
-button_container.appendChild(clear);
-button_container.appendChild(start);
+function newGame(){
+	document.getElementById("PGN-banner").textContent = '';
+	ending.style.display = "none";					//Initially hidden
+	game = new Chess();
 
-var curr_position = board.position('fen');
-console.log(curr_position);
+}
+
+function sendFen(){
+	var temp = game.fen();								//Gets the FEN from game instance
+	var data = {'fen' : temp };							//Package data
+	var httpRequest = new XMLHttpRequest();
+	httpRequest.open('POST', '/fen');
+	httpRequest.setRequestHeader('Content-Type', 'application/json');
+	httpRequest.send(JSON.stringify(data));				//Send the data to the server
+}
+
+function onDrop(source, target){
+
+	var move = game.move({		//returns an array of possible moves
+		from: source,
+		to: target,
+		promotion: 'q' 			//Promote pawns to Queens
+	})
+	
+	if (move === null) return 'snapback'	//If null, no possible moves
+}
+
+function onChange(oldPos, newPos){
+	var pgn_banner = document.getElementById("PGN-banner");
+	pgn_banner.textContent = game.pgn();
+	if (game.game_over()){
+		var ending = document.getElementById("ending-banner");
+		ending.style.display = "inherit";
+		if (game.in_checkmate()){
+			if (game.turn() === 'b'){
+				ending.textContent = "White wins by checkmate!";
+			} else {
+				ending.textContent = "Black wins by checkmate!";
+			}
+		} else {
+			ending.textContent = "Game is a draw!";
+		}
+	}
+
+}
+
+window.addEventListener("load", load, true);			//Initialize after page finishes loading
